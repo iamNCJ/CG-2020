d543fd82615f14bda115b511074953514ca29dae
NCJ <iamncj@outlook.com>
2020-10-26 09:40:24 +0800

    stash

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -1,89 +1,136 @@
-#include <cstdio>
-#include <GL/freeglut.h>
+#include <GL/glut.h>
+#include <cmath>
 
-int SCR_WIDTH = 800;
-int SCR_HEIGHT = 600;
+float angle = 0.0;
+float x = 0.0f, y = 1.75f, z = 5.0f;
+float lx = 0.0f, ly = 0.0f, lz = -1.0f;
 
-int mouseX, mouseY;
+void changeSize(int w, int h) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45, 1.0f * w / h, 1, 1000);
 
-void render() {
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 }
 
-static void display() {
+void drawSnowMan() {
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glTranslatef(0.0f, 0.75f, 0.0f);
+    glutSolidSphere(0.75f, 20, 20);
+
+    glTranslatef(0.0f, 1.0f, 0.0f);
+    glutSolidSphere(0.25f, 20, 20);
+
+    glPushMatrix();
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.05f, 0.10f, 0.18f);
+    glutSolidSphere(0.05f, 10, 10);
+    glTranslatef(-0.1f, 0.0f, 0.0f);
+    glutSolidSphere(0.05f, 10, 10);
+    glPopMatrix();
+
+    glColor3f(1.0f, 0.5f, 0.5f);
+    glRotatef(0.0f, 1.0f, 0.0f, 0.0f);
+    glutSolidCone(0.08f, 0.5f, 10, 2);
+}
+
+void renderScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    render();
+
+    glBegin(GL_QUADS);
+    glVertex3f(-100.0f, 0.0f, -100.0f);
+    glVertex3f(-100.0f, 0.0f, 100.0f);
+    glVertex3f(100.0f, 0.0f, 100.0f);
+    glVertex3f(100.0f, 0.0f, -100.0f);
+    glEnd();
+
+    for (int i = -3; i < 3; i++)
+        for (int j = -3; j < 3; j++) {
+            glPushMatrix();
+            glTranslatef(i * 10.0, 0, j * 10.0);
+            drawSnowMan();
+            glPopMatrix();
+        }
     glutSwapBuffers();
 }
 
-static void reshape(int w, int h) {
-    SCR_WIDTH = w;
-    SCR_HEIGHT = h;
-    printf("reshape\n");
+void orientMe(float ang) {
+    lx = sin(ang);
+    lz = -cos(ang);
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 }
 
-static void drag(int x, int y) {
-    printf("drag\n");
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
+void move_Front_Back(int direction) {
+    x = x + direction * (lx) * 0.1;
+    z = z + direction * (lz) * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 }
 
-static void click(int button, int state, int x, int y) {
-    printf("click:%d\n", button);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
+void move_Left_Right(int direction) {
+    x = x + direction * (lz) * 0.1;
+    z = z - direction * (lx) * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 }
 
-static void wheel(int wheel, int dir, int x, int y) {
-    printf("wheel:%d\n", wheel);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
+void move_High_Low(int direction) {
+    y = y + direction * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
 }
 
-static void keyboardDown(unsigned char key, int x, int y) {
-    printf("key down: %d\n", key);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
+//键盘响应
+void inputKey(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'q':
+            angle -= 0.03f;
+            orientMe(angle);
+            break;
+        case 'e':
+            angle += 0.03f;
+            orientMe(angle);
+            break;
+        case 'w':
+            move_Front_Back(1);
+            break;
+        case 's':
+            move_Front_Back(-1);
+            break;
+        case 'a':
+            move_Left_Right(1);
+            break;
+        case 'd':
+            move_Left_Right(-1);
+            break;
+        case '1':
+            move_High_Low(1);
+            break;
+        case '2':
+            move_High_Low(-1);
+            break;
+        default:
+            break;
+    }
 }
 
-static void keyboardUp(unsigned char key, int x, int y) {
-    printf("key up: %d\n", key);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
-}
-
-static void specialDown(int key, int x, int y) {
-    printf("special down: %d\n", key);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
-}
-
-static void specialUp(int key, int x, int y) {
-    printf("special up: %d\n", key);
-    printf("pos:(%d %d)\n", x, y);
-    glutPostRedisplay();
-}
-
-static void init(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
-    glutInitWindowSize(800, 600);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("My Solar System");
-    glClearColor(0.f, 0.f, 0.f, 1.0f);
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutMotionFunc(drag);
-    glutMouseFunc(click);
-    glutMouseWheelFunc(wheel);
-    glutKeyboardFunc(keyboardDown);
-    glutKeyboardUpFunc(keyboardUp);
-    glutSpecialFunc(specialDown);
-    glutSpecialUpFunc(specialUp);
-}
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(640, 360);
+    glutCreateWindow("snowman test");
 
-int main(int argc, char *argv[]) {
-    init(argc, argv);
+    glEnable(GL_DEPTH_TEST);
+    glutKeyboardFunc(inputKey);
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    glutReshapeFunc(changeSize);
     glutMainLoop();
     return 0;
 }
