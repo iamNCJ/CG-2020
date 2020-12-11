26f6b590087724b493a58596962ee730ff9bc1c9
NCJ <iamncj@outlook.com>
2020-12-11 13:14:01 +0800

    stash

---
+++ lab07/main.cpp
@@ -0,0 +1,115 @@
+/*  bezsurf.c
+ *  This program renders a wireframe Bezier surface,
+ *  using two-dimensional evaluators.
+ */
+#include <GL/glut.h>
+#include <stdlib.h>
+
+GLfloat ctrlpoints[4][4][3] = {
+        {{-1.5, -1.5, 4.0}, {-0.5, -1.5, 2.0},
+                {0.5, -1.5, -1.0}, {1.5, -1.5, 2.0}},
+        {{-1.5, -0.5, 1.0}, {-0.5, -0.5, 3.0},
+                {0.5, -0.5, 0.0}, {1.5, -0.5, -1.0}},
+        {{-1.5, 0.5, 4.0}, {-0.5, 0.5, 0.0},
+                {0.5, 0.5, 3.0}, {1.5, 0.5, 4.0}},
+        {{-1.5, 1.5, -2.0}, {-0.5, 1.5, -2.0},
+                {0.5, 1.5, 0.0}, {1.5, 1.5, -1.0}}
+};
+
+void display(void)
+{
+    int i, j;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix ();
+    glRotatef(85.0, 1.0, 1.0, 1.0);
+    for (j = 0; j <= 8; j++) {
+        glBegin(GL_LINE_STRIP);
+        for (i = 0; i <= 30; i++)
+            glEvalCoord2f((GLfloat)i/30.0, (GLfloat)j/8.0);
+        glEnd();
+        glBegin(GL_LINE_STRIP);
+        for (i = 0; i <= 30; i++)
+            glEvalCoord2f((GLfloat)j/8.0, (GLfloat)i/30.0);
+        glEnd();
+    }
+    //glEvalMesh2( GL_LINE, 0, 20, 0, 20 );
+    glPopMatrix ();
+    glFlush();
+}
+
+void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    /*
+     GL_MAP1_VERTEX_3 -- specifies that 3-dimensional control points are
+             provided and 3-D vertices should be produced
+     0   -- min u
+     1   -- max u
+     3   -- ustride, number of values to advance in the data between two
+        consecutive control points ( see diagram below )
+     4   -- order of u ( = degree + 1 )
+     0   -- min v
+     1   -- max v
+     12  -- vstride ( see diagram below )
+     4   -- order of v ( = degree + 1 )
+    */
+    glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4,
+            0, 1, 12, 4, &ctrlpoints[0][0][0]);
+    glEnable(GL_MAP2_VERTEX_3);
+    /*
+     glMapGrid2f() -- defines a grid going from u1 to u2; v1 to v2 in
+              evenly-spaced steps
+     20  --  number of u steps
+     0.0 --  u1 ( starting u )
+     1.0 --  u2 ( ending u )
+     20  --  number of v steps
+     0.0 --  v1 ( starting v )
+     1.0 --  v2 ( ending v )
+
+     use with glEvalMesh2()
+    */
+    //glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_FLAT);
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (w <= h)
+        glOrtho(-4.0, 4.0, -4.0*(GLfloat)h/(GLfloat)w,
+                4.0*(GLfloat)h/(GLfloat)w, -4.0, 4.0);
+    else
+        glOrtho(-4.0*(GLfloat)w/(GLfloat)h,
+                4.0*(GLfloat)w/(GLfloat)h, -4.0, 4.0, -4.0, 4.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+        case 27:
+            exit(0);
+            break;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow (argv[0]);
+    init ();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
---
+++ lab07/new-main.cpp
@@ -0,0 +1,137 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <GL/glut.h>
+#include <cmath>
+
+float angle = 0.0;
+float x = 0.0f, y = 1.75f, z = 5.0f;
+float lx = 0.0f, ly = 0.0f, lz = -1.0f;
+
+void changeSize(int w, int h) {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    gluPerspective(45, 1.0f * w / h, 1, 1000);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+}
+
+void drawSnowMan() {
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glTranslatef(0.0f, 0.75f, 0.0f);
+    glutSolidSphere(0.75f, 20, 20);
+
+    glTranslatef(0.0f, 1.0f, 0.0f);
+    glutSolidSphere(0.25f, 20, 20);
+
+    glPushMatrix();
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.05f, 0.10f, 0.18f);
+    glutSolidSphere(0.05f, 10, 10);
+    glTranslatef(-0.1f, 0.0f, 0.0f);
+    glutSolidSphere(0.05f, 10, 10);
+    glPopMatrix();
+
+    glColor3f(1.0f, 0.5f, 0.5f);
+    glRotatef(0.0f, 1.0f, 0.0f, 0.0f);
+    glutSolidCone(0.08f, 0.5f, 10, 2);
+}
+
+void renderScene(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    glVertex3f(-100.0f, 0.0f, -100.0f);
+    glVertex3f(-100.0f, 0.0f, 100.0f);
+    glVertex3f(100.0f, 0.0f, 100.0f);
+    glVertex3f(100.0f, 0.0f, -100.0f);
+    glEnd();
+
+    for (int i = -3; i < 3; i++)
+        for (int j = -3; j < 3; j++) {
+            glPushMatrix();
+            glTranslatef(i * 10.0, 0, j * 10.0);
+            drawSnowMan();
+            glPopMatrix();
+        }
+    glutSwapBuffers();
+}
+
+void orientMe(float ang) {
+    lx = sin(ang);
+    lz = -cos(ang);
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+}
+
+void move_Front_Back(int direction) {
+    x = x + direction * (lx) * 0.1;
+    z = z + direction * (lz) * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+}
+
+void move_Left_Right(int direction) {
+    x = x + direction * (lz) * 0.1;
+    z = z - direction * (lx) * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+}
+
+void move_High_Low(int direction) {
+    y = y + direction * 0.1;
+    glLoadIdentity();
+    gluLookAt(x, y, z, x + lx, y + ly, z + lz, 0.0f, 1.0f, 0.0f);
+}
+
+void inputKey(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'q':
+            angle -= 0.03f;
+            orientMe(angle);
+            break;
+        case 'e':
+            angle += 0.03f;
+            orientMe(angle);
+            break;
+        case 'w':
+            move_Front_Back(1);
+            break;
+        case 's':
+            move_Front_Back(-1);
+            break;
+        case 'a':
+            move_Left_Right(1);
+            break;
+        case 'd':
+            move_Left_Right(-1);
+            break;
+        case '1':
+            move_High_Low(1);
+            break;
+        case '2':
+            move_High_Low(-1);
+            break;
+        default:
+            break;
+    }
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(640, 360);
+    glutCreateWindow("My Solar System");
+
+    glEnable(GL_DEPTH_TEST);
+    glutKeyboardFunc(inputKey);
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutMainLoop();
+    return 0;
+}
