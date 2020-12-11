895761ae3f014f1779b96d85a137bc677b932baa
NCJ <iamncj@outlook.com>
2020-12-11 14:16:10 +0800

    Update main.cpp

--- lab07/main.cpp
+++ lab07/main.cpp
@@ -6,6 +6,7 @@
 #include <GL/glut.h>
 #endif
 #include <cstdlib>
+#include <cstdio>
 
 GLfloat ctrlPoints[4][4][3] = {
         {{-1.5, -1.5, 4.0}, {-0.5, -1.5, 2.0},
@@ -18,100 +19,211 @@ GLfloat ctrlPoints[4][4][3] = {
                 {0.5, 1.5, 0.0}, {1.5, 1.5, -1.0}}
 };
 
-void display()
-{
-    int i, j;
+int SCR_WIDTH = 800;
+int SCR_HEIGHT = 600;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glPushMatrix ();
-    glRotatef(85.0, 1.0, 1.0, 1.0);
-    for (j = 0; j <= 8; j++) {
-        glBegin(GL_LINE_STRIP);
-        for (i = 0; i <= 30; i++)
-            glEvalCoord2f((GLfloat)i/30.0, (GLfloat)j/8.0);
-        glEnd();
-        glBegin(GL_LINE_STRIP);
-        for (i = 0; i <= 30; i++)
-            glEvalCoord2f((GLfloat)j/8.0, (GLfloat)i/30.0);
-        glEnd();
+int lastMouseX, lastMouseY;
+int time = 0;
+
+float angle = 60.0;
+float cameraX = 0.0f, cameraY = 0.0f, cameraZ = 2.0f;
+float viewDirX = 0.0f, viewDirY = 0.0f, viewDirZ = -1.0f, lastViewDirX, lastViewDirY;
+
+bool isShiftDown, isWDown, isSDown, isADown, isDDown, isSpaceDown;
+
+void modeling() {
+    float angle1 = 1 * time;
+    float angle2 = 3 * time;
+    float angle3 = 2 * time;
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    // Center Star
+    glColor3f(0.0, 0.0, 1.0);
+    glutWireSphere(0.2, 50, 50);
+
+    glRotatef(angle1, 0, 1, 0);
+    {
+        glTranslated(0.6, 0, 0);
+        {
+            // Planet 1
+            glColor3f(1.0, 1.0, 1.0);
+            glutWireSphere(0.1, 50, 50);
+            glRotatef(angle2, 0, 1, 0.5);
+            {
+                glTranslated(0.2, 0, 0);
+                {
+                    glColor3f(1.0, 0.0, 0.0);
+                    // Satellite
+                    glutWireSphere(0.05, 50, 50);
+                }
+                glTranslated(-0.2, 0, 0);
+            }
+            glRotatef(-angle2, 0, 1, 0.5);
+        }
+        glTranslated(-0.6, 0, 0);
     }
-    //glEvalMesh2( GL_LINE, 0, 20, 0, 20 );
-    glPopMatrix ();
-    glFlush();
+    glRotatef(-angle1, 0, 1, 0);
+    glRotatef(angle3, 0, 0.5, 0.5);
+    {
+        glTranslated(0.4, 0, 0);
+        {
+            // Planet 2
+            glColor3f(0.0, 1.0, 0.0);
+            glutWireSphere(0.1, 50, 50);
+        }
+        glTranslated(-0.4, 0, 0);
+    }
+    glRotatef(-angle3, 0, 0.5, 0.5);
 }
 
-void init()
-{
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    /*
-     GL_MAP1_VERTEX_3 -- specifies that 3-dimensional control points are
-             provided and 3-D vertices should be produced
-     0   -- min u
-     1   -- max u
-     3   -- ustride, number of values to advance in the data between two
-        consecutive control points ( see diagram below )
-     4   -- order of u ( = degree + 1 )
-     0   -- min v
-     1   -- max v
-     12  -- vstride ( see diagram below )
-     4   -- order of v ( = degree + 1 )
-    */
-    glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4,
-            0, 1, 12, 4, &ctrlPoints[0][0][0]);
-    glEnable(GL_MAP2_VERTEX_3);
-    /*
-     glMapGrid2f() -- defines a grid going from u1 to u2; v1 to v2 in
-              evenly-spaced steps
-     20  --  number of u steps
-     0.0 --  u1 ( starting u )
-     1.0 --  u2 ( ending u )
-     20  --  number of v steps
-     0.0 --  v1 ( starting v )
-     1.0 --  v2 ( ending v )
-
-     use with glEvalMesh2()
-    */
-    //glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
-    glEnable(GL_DEPTH_TEST);
-    glShadeModel(GL_FLAT);
+static void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    modeling();
+    glLoadIdentity();
+    gluLookAt(cameraX, cameraY, cameraZ, cameraX + viewDirX, cameraY + viewDirY, cameraZ + viewDirZ, 0.0f, 1.0f, 0.0f);
+    glutSwapBuffers();
 }
 
-void reshape(int w, int h)
-{
-    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+static void reshape(int w, int h) {
+    SCR_WIDTH = w;
+    SCR_HEIGHT = h;
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if (w <= h)
-        glOrtho(-4.0, 4.0, -4.0*(GLfloat)h/(GLfloat)w,
-                4.0*(GLfloat)h/(GLfloat)w, -4.0, 4.0);
-    else
-        glOrtho(-4.0*(GLfloat)w/(GLfloat)h,
-                4.0*(GLfloat)w/(GLfloat)h, -4.0, 4.0, -4.0, 4.0);
+    glViewport(0, 0, w, h);
+    gluPerspective(angle, 1.0f * w / h, 0.1, 1000);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluLookAt(cameraX, cameraY, cameraZ, cameraX + viewDirX, cameraY + viewDirY, cameraZ + viewDirZ, 0.0f, 1.0f, 0.0f);
 }
 
-void keyboard(unsigned char key, int x, int y)
-{
+static void drag(int _x, int _y) {
+    viewDirX = 0.001 * (_x - lastMouseX) + lastViewDirX;
+    viewDirY = -0.001 * (_y - lastMouseY) + lastViewDirY;
+}
+
+static void click(int button, int state, int _x, int _y) {
+    lastMouseX = _x;
+    lastMouseY = _y;
+    lastViewDirX = viewDirX;
+    lastViewDirY = viewDirY;
+    glutPostRedisplay();
+}
+
+static void wheel(int wheel, int dir, int _x, int _y) {
+    glutPostRedisplay();
+    angle += dir * 1.0;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(angle, 1.0f * SCR_WIDTH / SCR_HEIGHT, 0.1, 1000);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+static void keyboardDown(unsigned char key, int _x, int _y) {
     switch (key) {
-        case 27:
-            exit(0);
+        case 'w':
+            isWDown = true;
+            break;
+        case 's':
+            isSDown = true;
+            break;
+        case 'a':
+            isADown = true;
+            break;
+        case 'd':
+            isDDown = true;
+            break;
+        case ' ':
+            isSpaceDown = true;
+            break;
+        default:
             break;
     }
 }
 
-int main(int argc, char** argv)
-{
+static void keyboardUp(unsigned char key, int x, int y) {
+    printf("%d\n", key);
+    switch (key) {
+        case 'w':
+            isWDown = false;
+            break;
+        case 's':
+            isSDown = false;
+            break;
+        case 'a':
+            isADown = false;
+            break;
+        case 'd':
+            isDDown = false;
+            break;
+        case ' ':
+            isSpaceDown = false;
+            break;
+        default:
+            break;
+    }
+}
+
+#ifdef WIN32
+static void specialDown(int key, int _x, int _y) {
+    if (key == GLUT_KEY_SHIFT_L || key == GLUT_KEY_SHIFT_R) {
+        isShiftDown = true;
+    }
+}
+
+static void specialUp(int key, int x, int y) {
+    if (key == GLUT_KEY_SHIFT_L || key == GLUT_KEY_SHIFT_R) {
+        isShiftDown = false;
+    }
+}
+#endif
+
+static void idle() {
+    time = glutGet(GLUT_ELAPSED_TIME) / 10;
+    glutPostRedisplay();
+    if (isShiftDown) {
+        cameraY -= 0.01;
+    }
+    if (isSpaceDown) {
+        cameraY += 0.01;
+    }
+    if (isWDown) {
+        cameraX += viewDirX * 0.01;
+        cameraZ += viewDirZ * 0.01;
+    }
+    if (isSDown) {
+        cameraX -= viewDirX * 0.01;
+        cameraZ -= viewDirZ * 0.01;
+    }
+    if (isADown) {
+        cameraX += viewDirZ * 0.01;
+        cameraZ -= viewDirX * 0.01;
+    }
+    if (isDDown) {
+        cameraX -= viewDirZ * 0.01;
+        cameraZ += viewDirX * 0.01;
+    }
+}
+
+int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow (argv[0]);
-    init ();
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
+    glutInitWindowSize(SCR_WIDTH, SCR_HEIGHT);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("My Solar System");
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.f, 0.f, 0.f, 1.0f);
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
+    glutMotionFunc(drag);
+    glutMouseFunc(click);
+#ifdef WIN32
+    glutMouseWheelFunc(wheel); // mouse wheel function is not supported in macOS
+    glutSpecialFunc(specialDown); // Shift key detection is not working in macOS
+    glutSpecialUpFunc(specialUp);
+#endif
+    glutKeyboardFunc(keyboardDown);
+    glutKeyboardUpFunc(keyboardUp);
+    glutIdleFunc(idle);
     glutMainLoop();
     return 0;
 }
